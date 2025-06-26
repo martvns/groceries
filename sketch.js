dccf1f8fa026dfddeaa11322687a4a110f45aa7b
martvns <mborowska14@st.swps.edu.pl>
2025-06-26 12:24:18 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,20 +11,20 @@ let koszykImg;
 let pickSound;
 let bgMusic;
 let pokazOkno = false;
-let czas = 20;
+let czas = 15;
 let licznikStart = false;
 let monetaImg;
 let pokazMonety = false;
 let wybranoMonete = false;
 let komunikat = "";
 let monety = [];
-let ukryteMonety = [
-  { x: 80, y: 250, size: 40, collected: false, visible: false },
-  { x: 510, y: 250, size: 40, collected: false, visible: false },
-  { x: 0, y: 0, size: 40, collected: false, visible: false, active: false }
-];
+let ukryteMonety = [];
 let ostatniZbiór = 0;
 let zebraneMonety = 0;
+let liczbaProbMonety = 0;
+let specjalnaOferta = false;
+let EkranGratulacje = false;
+let ekranPowitalny = true;
 
 
 function preload() {
@@ -42,6 +42,7 @@ function preload() {
   produktImg['donut'] = loadImage('donut.png');
   produktImg['monster'] = loadImage('monster.png');
   
+  //muzyka
   pickSound = loadSound('pickup.mp3');
   bgMusic = loadSound('bgsound.mp3');
 }
@@ -75,8 +76,16 @@ gracz = {
     speed: 4
   };
   
+ukryteMonety = [
+  { x: 80, y: 250, size: 40, collected: false, visible: false },
+  { x: 510, y: 250, size: 40, collected: false, visible: false },
+  { x: 0, y: 0, size: 40, collected: false, visible: false, active: false }
+];
+  
   ukryteMonety[0].visible = true;
   ukryteMonety[1].visible = true;
+  
+  ukryteMonety[2].cannotBeCaught = true;
   
   produkty = [
     { x: 500, y: 235, size: 40, name: 'cola' },
@@ -87,11 +96,56 @@ gracz = {
   ];
 }
 
+
 function draw() {
+  if (ekranPowitalny) {
+    rysujEkranPowitalny();
+    return;
+  }
+  
   if (ekranStartowy) {
     rysujEkranStartowy();
     return;
   }
+  
+  //ekran gratulacje po losowaniu
+  if (EkranGratulacje) {
+  background(hueValue, 80, 100);
+  hueValue = (hueValue + 1) % 360;
+
+  //gwiazdki
+  noStroke();
+  for (let g of gwiazdki) {
+    fill(60, 0, 100, g.alpha / 100); // białe, błyszczące
+    ellipse(g.x, g.y, g.size);
+    g.y += g.speed;
+    if (g.y > height) {
+      g.y = 0;
+      g.x = random(width);
+    }
+  }
+
+  let boxWidth = 500;
+  let boxHeight = 280;
+  let boxX = width / 2 - boxWidth / 2;
+  let boxY = height / 2 - boxHeight / 2;
+
+  fill(255);
+  stroke(0);
+  strokeWeight(4);
+  rect(boxX, boxY, boxWidth, boxHeight, 20);
+
+  noStroke();
+  fill(0);
+  textAlign(CENTER, CENTER);
+  textSize(28);
+let tekst = "Gratulacje! Wygrałeś nagrodę specjalną!" +
+    " Do Twojego koszyka zostało dodane członkostwo VIP w naszym sklepie internetowym!" + 
+ " Jego koszt to jedynie 999zł co miesiąc!";
+
+text(tekst, boxX + 20, boxY + 20, boxWidth - 40, boxHeight - 40);
+  return;
+}
   
   //tło
   image(tloImg, 0, 0, width, height);
@@ -132,27 +186,43 @@ noStroke();
 }
 
   // uciekająca moneta
-  if (ukryteMonety[2].active && !ukryteMonety[2].collected) {
-  // Poruszaj monetę z dala od gracza
+if (ukryteMonety[2].active && !ukryteMonety[2].collected) {
   let m = ukryteMonety[2];
   let dx = m.x - gracz.x;
   let dy = m.y - gracz.y;
   let distGracz = dist(m.x, m.y, gracz.x, gracz.y);
 
-  if (distGracz < 150) { // jeśli za blisko, ucieka
+  if (distGracz < 200) {
     let angle = atan2(dy, dx);
-    m.x += cos(angle) * 5;
-    m.y += sin(angle) * 5;
+    let speed = map(distGracz, 0, 200, 10, 2); // szybciej im bliżej
+    m.x += cos(angle) * speed;
+    m.y += sin(angle) * speed;
 
-    // ogranicz pozycję do ekranu
-    m.x = constrain(m.x, 0, 700,);
-    m.y = constrain(m.y, 100, 470,);
+    // czasem ucieka skokowo w losowe miejsce
+    if (random(1) < 0.02) {
+  let nowaX, nowaY;
+  let proby = 0;
+  do {
+    nowaX = m.x + random(-50, 50);
+    nowaY = m.y + random(-50, 50);
+    proby++;
+  } while (
+    (nowaX < 0 || nowaX > 700 || nowaY < 100 || nowaY > 470 || !nieKolizjaZGraczem({ x: nowaX, y: nowaY }))
+    && proby < 10
+  );
+
+  m.x = constrain(nowaX, 0, 700);
+  m.y = constrain(nowaY, 100, 470);
+}
+
+    m.x = constrain(m.x, 0, 700);
+    m.y = constrain(m.y, 100, 470);
   }
 
   image(monetaImg, m.x, m.y, m.size, m.size);
 }
   
-  // Rysuj produkty
+  // rysuj produkty
   for (let p of produkty) {
   if (produktImg[p.name]) {
     let img = produktImg[p.name];
@@ -169,7 +239,7 @@ noStroke();
   }
   }
 
-  // Sterowanie gracza
+  // sterowanie gracza
   if (keyIsDown(LEFT_ARROW)) gracz.x -= gracz.speed;
   if (keyIsDown(RIGHT_ARROW)) gracz.x += gracz.speed;
   if (keyIsDown(UP_ARROW)) gracz.y -= gracz.speed;
@@ -179,16 +249,16 @@ noStroke();
   gracz.y = constrain(gracz.y, 100, 470,);
 
   
-  // Produkty w koszyku
+  // produkty w koszyku
   for (let p of koszyk) {
   if (produktImg[p.name]) {
     if (p.isAnimating) {
       
-      // Przesuwaj produkt w stronę celu
+      // przesuwaj produkt w stronę celu
       p.x += (p.targetX - p.x) * 0.1;
       p.y += (p.targetY - p.y) * 0.1;
 
-      // Zatrzymanie animacji przy koszyku
+      // zatrzymanie animacji przy koszyku
       if (dist(p.x, p.y, p.targetX, p.targetY) < 1) {
         p.x = p.targetX;
         p.y = p.targetY;
@@ -211,7 +281,7 @@ noStroke();
   }
 }
   
-  // Koszyk
+  // koszyk
   if (koszykImg) {
   image(koszykImg, 590, 410, 150, 120); 
   }
@@ -230,25 +300,27 @@ noStroke();
       
   let tekst = "Niestety promocja się skończyła! Spróbuj następnym razem albo weź udział w specjalnym losowaniu już teraz!";
   let x = width / 2 - 180;
-  let y = height / 2 - 90;
+  let y = height / 2 - 60;
   let boxWidth = 360;
   let boxHeight = 180;
 
   text(tekst, x, y, boxWidth, boxHeight);
       
-// Rysuj przycisk "Weź udział"
+      
+      
+//przycisk "Weź udział"
 fill(hueValue, 80, 100);
       hueValue = (hueValue + 1) % 360;
 rect(width / 2 - 150, height / 2 + 120, 130, 40, 10);
 fill(0);
 textSize(18);
-text("Weź udział", width / 2 - 85, height / 2 + 140);
+text("Weź udział", width / 2 - 85, height / 2 + 130);
 
-// Rysuj przycisk "Innym razem"
+//przycisk "Innym razem"
 fill(100, 100, 100);
 rect(width / 2 + 20, height / 2 + 120, 130, 40, 10);
 fill(0);
-text("Innym razem", width / 2 + 85, height / 2 + 140);
+text("Innym razem", width / 2 + 85, height / 2 + 130);
 }
 
   //okno na monety
@@ -259,6 +331,13 @@ text("Innym razem", width / 2 + 85, height / 2 + 140);
   strokeWeight(4);
   rect(width / 2 - 250, height / 2 - 120, 500, 240, 20);
   noStroke();
+    
+    // podgląd liczby prób
+if (pokazMonety || wybranoMonete) {
+  fill(0);
+  textSize(22);
+  text("Szanse: " + (3 - liczbaProbMonety) + " / 3", width / 2, height / 2 - 100);
+}
 
   //monety
   for (let m of monety) {
@@ -271,28 +350,65 @@ text("Innym razem", width / 2 + 85, height / 2 + 140);
   if (wybranoMonete) {
   fill(0);
   textSize(24);
-  text(komunikat, width / 2, height / 2 + 100);
+  text(komunikat, width / 2, height / 2 + 80);
 
-  // Przycisk "Spróbuj ponownie"
+  // przycisk "Spróbuj ponownie"
   fill(hueValue, 80, 100);
       hueValue = (hueValue + 1) % 360;
   rect(width / 2 - 100, height / 2 + 150, 200, 40, 10);
   fill(0);
   textSize(18);
-  text("Spróbuj ponownie", width / 2, height / 2 + 170);
+  text("Spróbuj ponownie", width / 2, height / 2 + 160);
 }
   
-  //gracz - musi być zawsze na końcu
+  //gracz - musi być zawsze na końcu draw!
   image(wybranaPostac, gracz.x, gracz.y, gracz.size, gracz.size);
+}
+
+function rysujEkranPowitalny() {
+  
+background(hueValue, 80, 100);
+  hueValue = (hueValue + 1) % 360;
+  noStroke();
+  for (let g of gwiazdki) {
+    fill(60, 0, 100, g.alpha / 100);
+    ellipse(g.x, g.y, g.size);
+    g.y += g.speed;
+    if (g.y > height) {
+      g.y = 0;
+      g.x = random(width);
+    }
+  }
+
+  let boxWidth = 500;
+  let boxHeight = 280;
+  let boxX = width / 2 - boxWidth / 2;
+  let boxY = height / 2 - boxHeight / 2;
+
+  fill(255);
+  stroke(0);
+  strokeWeight(4);
+  rect(boxX, boxY, boxWidth, boxHeight, 20);
+  noStroke();
+  fill(0);
+  textAlign(CENTER, TOP);
+  textSize(16);
+  let instrukcja =
+    "Zrób zakupy z super promocją!\n\n" +
+    "Twoim zadaniem jest zebranie produktów oraz 3 ukrytych monet – masz na to 15 sekund!\n\n" +
+    "- Poruszaj się za pomocą strzałek.\n\n" +
+    "- Zbieraj produkty i monety naciskając spację.\n\n" +
+    "- Naciśnij spacje, aby wybrać postać i zacząć!";
+  text(instrukcja, boxX + 20, boxY + 40, boxWidth - 40, boxHeight - 40);
 }
 
 function rysujEkranStartowy() {
   
-  // Tęczowe tło
+  //tęczowe tło
   background(hueValue, 80, 100);
   hueValue = (hueValue + 1) % 360;
 
-  // Gwiazdki
+  //gwiazdki
   noStroke();
   for (let g of gwiazdki) {
     fill(60, 0, 100, g.alpha / 100); // białe, błyszczące
@@ -306,14 +422,14 @@ function rysujEkranStartowy() {
 
   fill(0);
   textSize(32);
-  text("Wybierz swoją postać", width / 2, 250);
+  text("Wybierz swoją postać", width / 2, 190);
 
-  image(postac1Img, width / 2 - 150, height/2, 100, 100);
-  image(postac2Img, width / 2 + 50, height/2, 100, 100);
+  image(postac1Img, width / 2 - 150, 250, 100, 150);
+  image(postac2Img, width / 2 + 50, 250, 100, 150);
 
   textSize(20);
-  text("Naciśnij 1", width / 2 - 100, 450);
-  text("Naciśnij 2", width / 2 + 100, 450);
+  text("Naciśnij 1", width / 2 - 100, 430);
+  text("Naciśnij 2", width / 2 + 100, 430);
 }
 
 function PrzykrytaMoneta(moneta) {
@@ -328,9 +444,19 @@ function PrzykrytaMoneta(moneta) {
   return false;
 }
 
+function nieKolizjaZGraczem(moneta) {
+  let margin = 50; // dystans minimalny między monetą a graczem
+  return dist(moneta.x, moneta.y, gracz.x + gracz.size / 2, gracz.y + gracz.size / 2) > margin;
+}
+
 function keyPressed() {
+  if (ekranPowitalny) {
+    ekranPowitalny = false;
+    ekranStartowy = true;
+    return;
+  }
   
-  if (millis() - ostatniZbiór < 300) return; //nie można zbierać paru naraz
+  if (millis() - ostatniZbiór < 300) return; 
   
   if (ekranStartowy) {
     if (key === '1') {
@@ -345,7 +471,7 @@ function keyPressed() {
 
   if (key === ' ') {
   if (pokazOkno && !pokazMonety) {
-    // SPRAWDŹ czy ludzik jest na przycisku „Innym razem”
+    //sprawdź czy ludzik jest na przycisku „Innym razem”
     let buttonX = width / 2 + 20;
     let buttonY = height / 2 + 120;
     let buttonW = 130;
@@ -358,14 +484,13 @@ function keyPressed() {
       gracz.y < buttonY + buttonH
     ) {
       
-      // Użytkownik wybrał „Innym razem” — reset do ekranu startowego
+      //użytkownik wybrał „Innym razem” — reset do ekranu startowego
       ekranStartowy = true;
       pokazOkno = false;
       pokazMonety = false;
       wybranoMonete = false;
       komunikat = "";
       koszyk = [];
-
       produkty = [
         { x: 500, y: 235, size: 40, name: 'cola' },
         { x: 360, y: 220, size: 40, name: 'cola2' },
@@ -373,6 +498,14 @@ function keyPressed() {
         { x: 620, y: 260, size: 40, name: 'donut' },
         { x: 70, y: 240, size: 40, name: 'monster' },
       ];
+      zebraneMonety = 0;
+      liczbaProbMonety = 0;
+      monety = [];
+      ukryteMonety = [
+    { x: 80, y: 250, size: 40, collected: false, visible: true },
+    { x: 510, y: 250, size: 40, collected: false, visible: true },
+    { x: 0, y: 0, size: 40, collected: false, visible: false, active: false, cannotBeCaught: true }
+  ];
       czas = 20;
       licznikStart = false;
       return;
@@ -403,21 +536,31 @@ function keyPressed() {
   }
 
     if (pokazMonety && !wybranoMonete) {
-  // Znajdź monetę, na którą najechał gracz
+  //znajdź monetę, na którą najechał gracz
   for (let m of monety) {
-    let dystans = dist(gracz.x + gracz.size / 2, gracz.y + gracz.size / 2, m.x, m.y);
-    if (dystans < 50) { // 50 = promień monety
-      m.kliknieta = true;
-      wybranoMonete = true;
+  let dystans = dist(gracz.x + gracz.size / 2, gracz.y + gracz.size / 2, m.x, m.y);
+  if (dystans < 50 && !m.kliknieta) {
+    m.kliknieta = true;
+    liczbaProbMonety++;
+
+    if (liczbaProbMonety === 3) {
+      
+      specjalnaOferta = true;
+      komunikat = "Gratulacje!";
+      EkranGratulacje = true;
+    } else {
       komunikat = "Brak nagrody!";
-      break;
     }
+
+    wybranoMonete = true;
+    break;
   }
+}
   return;
 }
 
 if (pokazMonety && wybranoMonete) {
-  // Sprawdź czy gracz stoi na przycisku „Spróbuj ponownie”
+  //sprawdź czy ludzik jest na przycisku „Spróbuj ponownie”
   let btnX = width / 2 - 100;
   let btnY = height / 2 + 150;
   let btnW = 200;
@@ -440,38 +583,38 @@ if (pokazMonety && wybranoMonete) {
   return;
 }
     
-    // sprawdzanie dla ukrytych monet
-    for (let i = 0; i < 3; i++) {
+    //sprawdzanie dla ukrytych monet
+for (let i = 0; i < 3; i++) {
   let m = ukryteMonety[i];
   if (m.visible && !m.collected) {
     let dystans = dist(gracz.x + gracz.size/2, gracz.y + gracz.size/2, m.x + m.size/2, m.y + m.size/2);
-    if (dystans < (gracz.size + m.size)/2 && !PrzykrytaMoneta(m)) {
+
+    
+    if (dystans < (gracz.size + m.size)/2 && !PrzykrytaMoneta(m) && !(m.cannotBeCaught)) {
       m.collected = true;
       zebraneMonety++;
       pickSound.play();
 
-      // Jeśli to 2 pierwsze monety - po zebraniu dwóch aktywuj trzecią
       if (i < 2) {
-        // Sprawdź ile jest już zebranych monet (2 pierwszych)
         let count = 0;
         if (ukryteMonety[0].collected) count++;
         if (ukryteMonety[1].collected) count++;
         if (count === 2) {
           ukryteMonety[2].active = true;
           ukryteMonety[2].visible = true;
-          // Umieść trzecią monetę w losowym miejscu
           ukryteMonety[2].x = random(100, width - 100);
           ukryteMonety[2].y = random(150, height - 150);
+          ukryteMonety[2].cannotBeCaught = true;  // moneta uciekająca nie do złapania spacją
         }
       }
-      break; // aby nie zbierać więcej monet na raz
+      break; 
     }
   }
 }
     
     ostatniZbiór = millis();
 
-    // dodawanie produktu do koszyka
+    //dodawanie produktu do koszyka
     for (let i = produkty.length - 1; i >= 0; i--) {
       let p = produkty[i];
       let dystans = dist(
